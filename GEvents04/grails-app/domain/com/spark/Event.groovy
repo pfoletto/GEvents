4e2106c9e8e4bbc6328a70f3ce51940591deb41e
paolo <paolo@NB111032>
2012-02-24 11:19:06 +0100

    commit unit 04 GrailsWorkshop

---
+++ GEvents04/grails-app/domain/com/spark/Event.groovy
@@ -0,0 +1,20 @@
+package com.spark
+
+class Event {
+   String name
+   String location
+   String venue
+   Date startDate
+   Date endDate
+   User organizer
+   String description
+   static constraints = {
+      name(blank:false)
+      location()
+      venue()
+      startDate()
+      endDate()
+      organizer()
+      description(maxSize:1000)
+   }
+}
